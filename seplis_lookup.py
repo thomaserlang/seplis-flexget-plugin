292fb9260556c3827bcc2f51aaef78bb074538cf
Thomas Erlang <thomas@erlang.dk>
2022-07-22 22:17:33 +0200

    Changed plugin priority

--- seplis_lookup.py
+++ seplis_lookup.py
@@ -33,7 +33,7 @@ class seplis_lookup:
         'movie_year': 'year',
     }
 
-    @plugin.priority(110)
+    @plugin.priority(130)
     def on_task_metainfo(self, task, config):
         if not config:
             return
