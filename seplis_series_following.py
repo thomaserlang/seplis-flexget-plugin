98d6bf60afdb59acdd341fe4739218c06757737b
Thomas Erlang <thomas@erlang.dk>
2022-07-25 01:40:11 +0200

    Removed year prenteces from series following

--- seplis_series_following.py
+++ seplis_series_following.py
@@ -45,7 +45,7 @@ class seplis_series_following:
                         entry = Entry()
                         entry['title'] = series['title']
                         year = int(series["premiered"][:4]) if series['premiered'] else None
-                        entry['title'] += f' ({year})' if year else ''
+                        entry['title'] += f' {year}' if year else ''
                         entry['seplis_year'] = year
                         entry['seplis_id'] = series['id']
                         entries.append(entry)
